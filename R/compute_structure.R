531ccd2ad45f41730eb902241b9d7faaa4b66673
Alexander Täuber <alexander.taeuber@idiv.de>
2026-03-23 19:31:37 +0100

    Update compute_structure

--- R/compute_structure.R
+++ R/compute_structure.R
@@ -1,71 +1,82 @@
 compute_structure <- function(
   cloud_path,
   tsq_name,
-  dtm_stats = FALSE,
+  aspect = FALSE,
   dtm_path = NULL,
   boxdim_t = 0.1,
   enl_layer_size = 1,
   chs_res = 0.1,
+  chs_rnd = 1L,
+  enl_rnd = 3L,
+  boxdim_rnd = 4L,
+  slope_rnd = 1L,
+  asp_rnd = 0L,
   plot = TRUE,
+  type = c("ln", "tsq"),
   verbose = TRUE
 ) {
   if (isTRUE(verbose)) message("Processing: ", cloud_path)
 
+  type <- toupper(match.arg(type))
+  plot_title <- glue::glue("{type}_{tsq_name}")
+
   cloud <- rlas::read.las(cloud_path, "xyz") |>
     coi::as_pt_cld()
 
-  boxdim <- coi::boxdim(
+  boxdim_res <- coi::boxdim(
     cloud,
     threshold = boxdim_t,
+    plot = plot,
+    plot_title = plot_title,
     warnings = FALSE
   )
 
-  enl <- coi::enl(
+  enl_res <- coi::enl(
     cloud,
     layer_thickness = enl_layer_size,
     plot = plot,
-    plot_title = tsq_name,
+    plot_title = plot_title,
     warnings = FALSE
   )
 
-  chs <- coi::canopy_stats(
+  chs_res <- coi::canopy_stats(
     cloud,
     res = chs_res,
     lower_cutoff = NULL,
     plot = plot,
-    plot_title = tsq_name
+    plot_title = plot_title
   )
 
-  if (dtm_stats && !is.null(dtm_path)) {
-    dtm <- Rvcg::vcgPlyRead(dtm_path)
+  dtm <- Rvcg::vcgPlyRead(dtm_path)
+  if (aspect) {
     dtm_stats <- coi::extract_slope(dtm, aspect = TRUE)
-    slope <- dtm_stats$slope
     aspect <- dtm_stats$aspect
   } else {
-    slope <- NA_real_
+    dtm_stats <- coi::extract_slope(dtm, aspect = FALSE)
     aspect <- NA_real_
   }
+  slope <- dtm_stats$slope
 
   results <- tibble::tibble(
     tsq = tsq_name,
-    chs_max = chs$max,
-    chs_mean = chs$mean,
-    chs_sd = chs$sd,
-    chs_cv = chs$cv,
-    chs_gini = chs$gini,
-    enl0 = enl$ENL0,
-    enl1 = enl$ENL1,
-    enl2 = enl$ENL2,
-    boxdim = boxdim,
-    slope = slope,
-    aspect = aspect
+    chs_max = round(chs_res$max, chs_rnd),
+    chs_mean = round(chs_res$mean, chs_rnd),
+    chs_sd = round(chs_res$sd, chs_rnd),
+    chs_cv = round(chs_res$cv, chs_rnd),
+    chs_gini = round(chs_res$gini, chs_rnd),
+    enl0 = enl_res$ENL0,
+    enl1 = round(enl_res$ENL1, enl_rnd),
+    enl2 = round(enl_res$ENL2, enl_rnd),
+    boxdim = round(boxdim_res$boxdim, boxdim_rnd),
+    slope = round(slope, slope_rnd),
+    aspect = round(aspect, asp_rnd)
   )
   if (plot) {
     results <- results |>
       dplyr::mutate(
-        chs_plot = list(chs$plot),
-        enl_plot = list(enl$plot),
-        boxdim_plot = list(boxdim$plot)
+        chs_plot = list(chs_res$plot),
+        enl_plot = list(enl_res$plot),
+        boxdim_plot = list(boxdim_res$plot)
       )
   }
   results
